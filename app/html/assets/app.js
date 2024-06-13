5d38f0d7dfcdb7f4d7f785b03f9d557f72408143
msk_z <msk_z@ptplus.jp>
2024-06-13 18:55:27 +0900

    chore: Update index.html with modal for phone number authentication

---
+++ app/html/assets/app.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', async () => {
+    document.getElementById('entry').addEventListener('click', (event) => {
+      let phone = document.getElementById('phone').value;        
+      let events = document.getElementById('events');
+
+      if (!phone) {
+        return;
+      }
+
+      controller = new AbortController();
+      const signal = controller.signal;
+      fetch(`${url}/api/v1/entry/${phone}`, {
+        method: 'GET',
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${token}`,
+        },
+        signal: signal,
+      }).then(res => {
+        const reader = res.body.getReader();
+        const decoder = new TextDecoder();
+        reader.read().then(async function progress({ done, value }) {
+          if (done) {
+            console.log('Stream complete');
+            return;
+          }
+          const data = JSON.parse(decoder.decode(value).replace(/^data: /, '').trim());
+          console.log(data);
+          events.innerHTML += `<div>${JSON.stringify(data, null, 2)}</div>`;
+          return reader.read().then(progress);
+        });
+      });
+
+    });
+
+    const applyModal = document.getElementById('applyModal');
+    applyModal.addEventListener('hidden.bs.modal', (event) => {
+      controller.abort();
+    });  });
